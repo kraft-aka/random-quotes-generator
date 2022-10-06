fcd8a5466b5fc003d0b946f31ae638de7cb67b60
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-10-06 17:24:38 +0200

    add useRef hook to change background

--- src/components/GetQuotes.js
+++ src/components/GetQuotes.js
@@ -1,9 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './style.css'
+
 
 const GetQuotes = () => {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
+  const colorRef = useRef(null);
+
+  const COLORS = [
+    '#ffd6ff',
+    '#f4acb7',
+    '#fefae0',
+    '#f0e6ef',
+    '#ccff33',
+    '#2667ff'
+  ]
 
   const url =
     "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
@@ -23,8 +34,12 @@ const GetQuotes = () => {
     getRandomQuote();
   }, []);
 
+  useEffect(()=> {
+    colorRef.current.style.backgroundColor = COLORS[Math.floor(Math.random() * COLORS.length)];
+  }, [quote])
+
   return (
-    <div className="quote-container">
+    <div className="quote-container" ref={colorRef}>
       <p id="quote">{quote}</p>
       <p id="author">Author: {author}</p>
       <button onClick={getRandomQuote}>Generate new</button>
